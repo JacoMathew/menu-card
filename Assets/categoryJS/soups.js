08329f138ba8e6ae231dba7a9ecb7356d2cfdeae
Jaco Mathew <appsbyjaco@gmail.com>
2020-11-26 00:13:27 +0530

    Structure updated, cards aligned and some styling and handlebar query

--- Assets/categoryJS/soups.js
+++ Assets/categoryJS/soups.js
@@ -1,63 +1,62 @@
-var soups = [
-    {
-        name : "Sweet Corn Chicken Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "99.00",
-        class : "NonVeg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+var soups = [{
+        name: "Sweet Corn Chicken Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "99.00",
+        class: "NonVeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Cream of Chicken Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "NonVeg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Cream of Chicken Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "NonVeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Sweet Corn Veg Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "Veg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Sweet Corn Veg Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "Veg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Cream of Veg Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "Veg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Cream of Veg Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "Veg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Cream of Tomato Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "Veg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Cream of Tomato Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "Veg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Hot and Sour Chicken Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "NonVeg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Hot and Sour Chicken Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "NonVeg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
     {
-        name : "Hot and Sour Veg Soup",
-        category : "Soup",
-        photo : "http://placehold.it/100",
-        price : "120.00",
-        class : "Veg",
-        description : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        name: "Hot and Sour Veg Soup",
+        category: "Soup",
+        photo: "http://placehold.it/100",
+        price: "120.00",
+        class: "Veg",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
     },
 ];
 
-$(document).ready(function(){
+$(document).ready(function() {
     const soupsTemplateHTML = $("#soups-template").html();
     const compliedHTML = Handlebars.compile(soupsTemplateHTML);
     const compliedSoupObject = compliedHTML(soups);
--- index.js
+++ index.js
@@ -4,9 +4,34 @@
 // });
 
 
-$(".card-footer").click(function(event) {
-    alert("Description");
-    // $(".descriptionToggle").toggleClass("d-flex").slideToggle('1000', 'linear');
-    // // If it is visible, then change the icon of the gallary to upwards arrow
-    // $("#upDownIcon").toggleClass('fa-angle-double-up fa-angle-double-down');
+// $(".card-footer").click(function(event) {
+//     alert("Description");
+// $(".descriptionToggle").toggleClass("d-flex").slideToggle('1000', 'linear');
+// // If it is visible, then change the icon of the gallary to upwards arrow
+// $("#upDownIcon").toggleClass('fa-angle-double-up fa-angle-double-down');
+// });
+
+$(document).on('click', '.card-footer', function () {
+    var footerId = $(this).attr("id");
+    var descriptionId = "#" + footerId + "description"
+    var arrowIconId = "#" + footerId + "arrow"
+
+    var msg = $(arrowIconId).attr('id');
+
+    console.log(msg);
+
+    // $(arrowIconId).toggleClass('fa-angle-double-up fa-angle-double-down');
+    // $(arrowIconId).addClass('fa-angle-double-up');
+
+    
+    $(descriptionId).slideToggle('1000', 'linear');
 });
+
+
+// ----------------
+
+// $("#certificate").click(function (event) {
+//     $("#certificates").toggleClass("d-flex").slideToggle('1000', 'linear');
+//     // If it is visible, then change the icon of the gallary to upwards arrow
+//     $("#upDownIcon").toggleClass('fa-angle-double-up fa-angle-double-down');
+//   });
